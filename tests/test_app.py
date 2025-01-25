f388c51a9708d9f99c5237969d59b8e76d625e64
guiribeirodev <guiribeirodev@gmail.com>
2025-01-24 22:38:41 -0300

    Aula 05 - Atualizando endpoints para usar o banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -27,37 +29,35 @@ def test_create_user(client):
 
 
 def test_read_users(client):
+    response = client.get('/users')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
     response = client.get('/users/')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': [user_schema]}
 
 
-def test_find_user(client):
-    response = client.get('/users/1')
+# def test_find_user(client):
+#     response = client.get('/users/1')
 
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'id': 1,
-        'username': 'alice',
-        'email': 'alice@example.com',
-    }
+#     assert response.status_code == HTTPStatus.OK
+#     assert response.json() == {
+#         'id': 1,
+#         'username': 'alice',
+#         'email': 'alice@example.com',
+#     }
 
-    response_fail = client.get('/users/100')
+#     response_fail = client.get('/users/100')
 
-    assert response_fail.status_code == HTTPStatus.NOT_FOUND
-    assert response_fail.json() == {'detail': 'User not found'}
+#     assert response_fail.status_code == HTTPStatus.NOT_FOUND
+#     assert response_fail.json() == {'detail': 'User not found'}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -86,7 +86,32 @@ def test_update_user(client):
     assert response_fail.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_update_integrity_error(client, user):
+    client.post(
+        '/users',
+        json={
+            'username': 'fausto',
+            'email': 'fausto@example.com',
+            'password': 'secret',
+        },
+    )
+
+    response_update = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'fausto',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response_update.status_code == HTTPStatus.CONFLICT
+    assert response_update.json() == {
+        'detail': 'Username or email already exists'
+    }
+
+
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
